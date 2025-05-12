672224f93c2d6d9ae95aa907ac31e53110e815ef
Collin-Schmitt <collinanglese23@gmail.com>
2025-05-12 13:13:00 -0500

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Collin Schmitt on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
